f3c268f6bc7fc4a6d6341e47cd401756ff4b1d70
Nikita Golovanov <cell-26@yandex.ru>
2020-03-25 03:04:50 +0300

    Remove api key

--- app/api/news.py
+++ app/api/news.py
@@ -1,10 +1,10 @@
-API_KEY = "79f2703915ea40e5982561af7a2147b9"
+from settings import NEWSAPI_API_KEY as API_KEY
 #https://newsapi.org/docs/client-libraries/python
 import requests
 
 url = ('http://newsapi.org/v2/top-headlines?'
        'country=us&'
-       'apiKey=79f2703915ea40e5982561af7a2147b9')
+       'apiKey=' + API_KEY)
 
 respounse = requests.get(url)
 if __name__ == "__main__":
